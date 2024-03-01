f8cbf619274a45777b9b1d6073a90cc0e068e09a
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-03-01 15:05:38 +0000

    feat: pass parentId as null in top level comments

---
+++ src/comment_section/components/CommentThread.tsx
@@ -0,0 +1,22 @@
+import { CommentThreadProps, Comment } from "@/comment_section/utils/types";
+import CommentBlock from "./CommentBlock";
+
+export default function CommentThread({
+  allComments,
+  setAllComments,
+}: CommentThreadProps) {
+  return (
+    <div className="flex flex-col gap-10 grow overflow-y-auto w-full pr-2">
+      {allComments.length > 0 &&
+        allComments.map((comment: Comment) => (
+          <CommentBlock
+            allComments={allComments}
+            key={comment.id}
+            comment={comment}
+            setAllComments={setAllComments}
+            parentId={null}
+          />
+        ))}
+    </div>
+  );
+}
