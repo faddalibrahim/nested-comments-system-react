49b573c6e66950b50e6c14cc980cf20ac56619c9
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-03-01 10:46:49 +0000

    style: move comment text to bottom of image

--- src/comment_section/components/CommentBlock.tsx
+++ src/comment_section/components/CommentBlock.tsx
@@ -8,6 +8,7 @@ import { CommentBlockProps } from "@/comment_section/utils/types";
 import {
   convertToLocaleDate,
   deleteComment,
+  updateComment,
 } from "@/comment_section/utils/functions";
 
 // components
@@ -22,23 +23,33 @@ export default function CommentBlock({
 
   return (
     <div className=" py-7 bg-[rgba(178,178,238,0.1)] rounded-md p-5">
-      <div className="flex gap-4 w-full">
-        <div className="w-[4rem] h-[4rem] flex items-center justify-center rounded-full border border-[0.2rem] border-[rgba(178,178,238,1)]">
-          <img
-            src="https://i.pravatar.cc"
-            alt=""
-            className="rounded-full w-full h-full"
-          />
+      <div className="flex justify-between gap-4 flex-wrap">
+        <div className="flex gap-3 justify-start items-start">
+          <div className="w-[3rem] h-[3rem] rounded-full border border-[0.2rem] border-[rgba(178,178,238,1)]">
+            <img
+              src="https://i.pravatar.cc"
+              alt=""
+              className="rounded-full w-full h-full"
+            />
+          </div>
+          <div className="flex flex-col text-white gap-0">
+            <div className="flex gap-2 items-center">
+              <p className="text-white text-lg">{comment.user.name}</p>
+              <small className="text-gray-600 font-medium">
+                {convertToLocaleDate(
+                  comment.isModified ? comment.modifiedAt : comment.createdAt
+                )}
+              </small>
+              {comment.isModified && (
+                <small className="text-gray-600 font-medium">editted</small>
+              )}
+            </div>
+            <small className="text-gray-600">@GojoSatoru</small>
+          </div>
         </div>
         <div className="flex flex-col gap-4 grow">
-          <div className="flex items-center text-white gap-3">
-            <p className="text-white text-xl">{comment.user.name}</p>
-            <small className="text-gray-600 font-medium">
-              {convertToLocaleDate(comment.createdAt)}
-            </small>
-          </div>
           {showEditBox ? (
-            <div className="flex flex-col gap-2 items-end w-full">
+            <div className="flex flex-col gap-2 items-end">
               <textarea
                 className="bg-[rgba(178,178,238,0.05)] text-white p-4 outline-0 border-0 ring-0 resize-none rounded-md w-full"
                 placeholder="type comment"
@@ -55,7 +66,13 @@ export default function CommentBlock({
                 >
                   cancel
                 </button>
-                <button className="bg-[rgba(59,53,70,1)] font-medium text-[#f3f5f7] px-3 py-1 rounded">
+                <button
+                  className="bg-[rgba(59,53,70,1)] font-medium text-[#f3f5f7] px-3 py-1 rounded"
+                  onClick={() => {
+                    updateComment(setAllComments, comment.id, commentEditText);
+                    setShowEditBox(false);
+                  }}
+                >
                   save
                 </button>
               </div>
