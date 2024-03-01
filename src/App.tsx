dc975c2f81edc7d27432d10a99bff50cc1d9c0f0
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-03-01 10:22:47 +0000

    style: style app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import CommentSection from "./comment_section/CommentSection";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-[#0A0A0E] h-[100svh] w-screen flex justify-center py-5">
+      <CommentSection />
+    </div>
+  );
 }
 
 export default App;
